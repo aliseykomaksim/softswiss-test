b58030a02394ee66c6939ac0547595bcec7aaa62
Aliseika Maksim <aliseykomaksim@gmail.com>
2025-09-13 20:32:26 +0200

    update readme, fix title

--- src/helpers/getImageSet.js
+++ src/helpers/getImageSet.js
@@ -6,7 +6,6 @@ export default function getImageSet(src) {
   const base = src.slice(0, -match[0].length);
   const ext = match[1].toLowerCase();
   const mime = ext === 'png' ? 'image/png' : 'image/jpeg';
-  console.log(base, ext, mime);
   return `image-set(
     url('${base}.webp') type('image/webp') 1x,
     url('${base}.${ext}') type('${mime}') 1x
